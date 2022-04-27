450a820248bac78f14e351cae145d621095d134c
standen1 <standen.sean@gmail.com>
2022-04-27 08:33:22 -0700

    Updated the dice.py file to import Dice class and removed old code.

--- dice.py
+++ dice.py
@@ -13,58 +13,8 @@
 #   https://realpython.com/python-dice-roll/
 
 #imports
-import random
-from turtle import width
+from dice_class import Dice
 
-#Global Constants
-#DICE_ART stores all of the ASCII art for the dice.
-DICE_ART = {
-    1: (
-        "┌─────────┐",
-        "│         │",
-        "│    ●    │",
-        "│         │",
-        "└─────────┘",
-    ),
-    2: (
-        "┌─────────┐",
-        "│  ●      │",
-        "│         │",
-        "│      ●  │",
-        "└─────────┘",
-    ),
-    3: (
-        "┌─────────┐",
-        "│  ●      │",
-        "│    ●    │",
-        "│      ●  │",
-        "└─────────┘",
-    ),
-    4: (
-        "┌─────────┐",
-        "│  ●   ●  │",
-        "│         │",
-        "│  ●   ●  │",
-        "└─────────┘",
-    ),
-    5: (
-        "┌─────────┐",
-        "│  ●   ●  │",
-        "│    ●    │",
-        "│  ●   ●  │",
-        "└─────────┘",
-    ),
-    6: (
-        "┌─────────┐",
-        "│  ●   ●  │",
-        "│  ●   ●  │",
-        "│  ●   ●  │",
-        "└─────────┘",
-    ),
-}
-DIE_HEIGHT = len(DICE_ART[1])
-DIE_WIDTH = len(DICE_ART[1][0])
-DIE_FACE_SEPARATOR = " "
 
 #Function: main
 #   Arguments: N/A
@@ -85,8 +35,8 @@ def main():
         if number_dice_prompt == -1:
             number_dice_prompt = prompt_user()
         else:
-            dice_roll = roll_dice(number_dice_prompt)
-            dice_face_diagram = generate_dice_faces(dice_roll)
+            dice_roll = Dice(number_dice_prompt)
+            dice_face_diagram = dice_roll.generate_dice_faces()
             print(f"\n{dice_face_diagram}")
             number_dice_prompt = prompt_user()
     print("Thanks for playing.  See you next time!!!")
@@ -117,74 +67,7 @@ def prompt_user():
         print("Your input is invalid.  Please try again.")
         return -1
 
-
-#Function: roll_dice
-#   Arguments: number_of_dice
-#   Variables: roll_results, roll
-#   Return: roll_results - A list of random integers each between 
-#       1 and 6 for each dice.
-#   Calls: N/A
-#   Called By: main
-def roll_dice(number_of_dice):
-    roll_results = []
-    i = 0
-    while i < number_of_dice:
-        roll = random.randint(1, 6)
-        roll_results.append(roll)
-        i = i + 1
-     
-    return roll_results
-
-#Function: get_dice_faces
-#   Arguments: dice_values
-#   Variables: dice_faces
-#   Return: dice_faces: a list that contains all of the 
-#       art for each dice.
-#   Calls: N/A
-#   Called By: generate_dice_faces
-def get_dice_faces(dice_values):
-    dice_faces = []
-    for value in dice_values:
-        dice_faces.append(DICE_ART[value])
-    return dice_faces
-
-#Function: get_dice_faces_rows
-#   Arguments: dice_faces
-#   Variables: dice_faces_rows, row_component, row_string
-#   Return: dice_faces_rows - A list with each row formatted for dice face.
-#   Calls: N/A
-#   Called By: generate_dice_faces
-def get_dice_faces_rows(dice_faces):
-    dice_faces_rows = []
-
-    for row_index in range(DIE_HEIGHT):
-        row_components = []
-        for die in dice_faces:
-            row_components.append(die[row_index])
-        row_string = DIE_FACE_SEPARATOR.join(row_components)
-        dice_faces_rows.append(row_string)
-
-    return dice_faces_rows
-
-#Function: generate_dice_faces
-#   Arguments: dice_values
-#   Variables: dice_faces, dice_faces_rows, width, diagram_header dice_faces_diagram
-#   Return: dice_faces_diagram - A string formatted for printing of each die's face.
-#   Calls: get_dice_faces, get_dice_faces_rows
-#   Called By: main
-def generate_dice_faces(dice_values):
-    dice_faces = get_dice_faces(dice_values)
-    dice_faces_rows = get_dice_faces_rows(dice_faces)
-
-    #Generate a header for Results
-    width = len(dice_faces_rows[0])
-    diagram_header = "RESULTS".center(width, "~")
-
-    #Generate diagram
-    dice_faces_diagram = "\n".join([diagram_header] + dice_faces_rows)
-    return dice_faces_diagram
-
-#Run main
+#Call main
 main()
 
 #Function info template:
